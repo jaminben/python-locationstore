7de095503d014b38869e6b21d5e124ebf73159bd
jaminben <jaminben@3ff1d020-1051-0410-9359-9732050ff79b>
2008-07-03 01:38:11 +0000

    dsdsf

--- trunk/src/test_interface.py
+++ trunk/src/test_interface.py
@@ -47,13 +47,27 @@ class BenInterfaceTestCase(unittest.TestCase):
   def setUp(self):
       self.filename = self.__class__.__name__ + '.db'
       self.homeDir = get_new_environment_path()
+      self.homeDir1 = get_new_environment_path()
+      self.homeDir2 = get_new_environment_path()
+            
       self.a = None
+      self.b = None
+      self.c = None
       
   def tearDown(self):
       if(self.a):
         self.a.close()
-        
+
+      if(self.b):
+        self.b.close()
+      
+      if(self.c):
+        self.c.close()
+                  
       test_support.rmtree(self.homeDir)
+      test_support.rmtree(self.homeDir1)
+      test_support.rmtree(self.homeDir2)
+      
 
   def testSimpleLocal(self):
     print self.homeDir
@@ -94,6 +108,54 @@ class BenInterfaceTestCase(unittest.TestCase):
     assert (back['bob'] == input_data['bob'] )
     
     self.a.close()  
+
+  def testReplicated(self):
+    print self.homeDir
+
+    REMOTE_HOST = "127.0.0.1"
+    REMOTE_PORT = 9001
+    
+    LOCAL_HOST  = "127.0.0.1"
+    LOCAL_PORT  = 9000
+
+    RREMOTE_HOST = "127.0.0.1"
+    RREMOTE_PORT = 9001
+
+
+    br = BDB_Replicated( LOCAL_HOST, LOCAL_PORT, True, 10, [ [REMOTE_HOST,REMOTE_PORT],  [RREMOTE_HOST,RREMOTE_PORT] ] )
+    
+    brr  = BDB_Replicated( REMOTE_HOST,REMOTE_PORT, False, 10, [ [LOCAL_HOST, LOCAL_PORT] ] )
+    
+    brr2 = BDB_Replicated( RREMOTE_HOST, RREMOTE_PORT, False, 10, [ [LOCAL_HOST, LOCAL_PORT] ] )
+    
+    self.a = SimpleLogDB( br )
+    self.a.open(self.homeDir)
+
+    self.b = SimpleLogDB( brr )
+    self.b.open(self.homeDir1)
+
+    self.c = SimpleLogDB( brr2 )
+    self.c.open(self.homeDir2)
+
+    # do some stuff here
+    input_data = {'bob' : 1, 'rad' : 2, 'timestamp' : time.time() }
+    rec_num = self.a.append( input_data )
+
+    back= self.a.get(rec_num)
+
+    assert (back['bob'] == input_data['bob'] )
+
+    back= self.b.get(rec_num)
+    assert (back['bob'] == input_data['bob'] )
+
+    back= self.c.get(rec_num)
+    assert (back['bob'] == input_data['bob'] )
+
+
+    self.a.close()
+    self.b.close()
+    self.c.close()
+
 
 #----------------------------------------------------------------------
 
